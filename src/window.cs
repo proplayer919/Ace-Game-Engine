107704699ed063c1571d2816f9eda1ae9f94de53
PRO PLAYER <95329795+proplayer919@users.noreply.github.com>
2022-11-05 19:05:51 +1100

    Add Files

---
+++ src/window.cs
@@ -0,0 +1,17 @@
+using System.Window;
+
+namespace Ace.Window
+{
+    class App ()
+    {
+        public static void Main()
+        {
+            // Initialize window
+Window window = new Window();
+
+// Show window modelessly
+// NOTE: Returns without waiting for window to close
+window.Show();
+        }
+    }
+}
